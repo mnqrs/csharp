95e27778c372091975ca3b968df0485e43e96d0c
mnqrs <116473798+mnqrs@users.noreply.github.com>
2023-08-23 16:00:51 -0300

    Calculo

--- Aula02/Calculo/Program.cs
+++ Aula02/Calculo/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            float num1, num2, num3, num4;
+
+            Console.WriteLine("Insira o primeiro número: ");
+            num1 = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Insira o segundo número: ");
+            num2 = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Insira o terceiro número: ");
+            num3 = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Insira o quarto número: ");
+            num4 = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("O resultado é: " + ((num1 * num2) - (num3 * num4)));
+
         }
     }
 }
