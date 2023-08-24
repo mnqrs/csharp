4ee73f822945f88fa19ab7518213752e58015653
mnqrs <116473798+mnqrs@users.noreply.github.com>
2023-08-24 13:40:25 -0300

    DoarSangue

---
+++ Aula03/DoarSangue/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace DoarSangue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            int idade;
+            bool sim = true;
+            bool nao = false;
+
+            Console.WriteLine("Digite o nome da pessoa doadora: ");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Digite a idade da pessoa doadora: ");
+            idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Primeira doação de sangue? ");
+            sim = Convert.ToBoolean(Console.ReadLine());
+           
+
+            if (idade >= 60 && sim == true)
+            {
+                Console.WriteLine($"{nome} não está apte a doar sangue!");
+            } 
+            
+            if (idade >= 18 && idade <= 69 && nao == false)
+            {
+                Console.WriteLine($"{nome} está apte a doar sangue!");
+            }
+
+            if (idade <= 17)
+            {
+                Console.WriteLine($"{nome} não está apte a doar sangue!");
+            }
+        }
+    }
+}
---
+++ Aula03/PlanoDeSaude/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace PlanoDeSaude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            int idade;
+
+            Console.WriteLine("Digite o seu nome: ");
+            nome = Console.ReadLine();  
+
+            Console.WriteLine($"Olá, {nome}! ");
+
+            Console.WriteLine("Digite a sua idade: ");
+            idade = Convert.ToInt32(Console.ReadLine);
+
+
+        }
+    }
+}
