36b74b252789681f7a91b583ac1c00f8c16a812c
mnqrs <116473798+mnqrs@users.noreply.github.com>
2023-08-24 13:59:39 -0300

    Par positivo

---
+++ Aula03/ParPositivo/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ParPositivo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.WriteLine("Digite o número: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero % 2 == 0 && numero > 0)
+            {
+                Console.WriteLine("O número é par e positivo");
+            }
+            if (numero % 2 == 0 && numero < 0)
+            {
+                Console.WriteLine("O número é par e negativo");
+            }
+            if (numero % 2> 0 && numero > 0)
+            {
+                Console.WriteLine("O número é ímpar e positivo");
+            }
+            if(numero % 2 < 0 && numero < 0)
+            {
+                Console.WriteLine("O número é ímpar e negativo");
+            }
+
+
+
+        }
+    }
+}
