a4f0067d09737847a720c85c30e3e166b7af340a
mnqrs <116473798+mnqrs@users.noreply.github.com>
2023-08-24 15:35:34 -0300

    calculadora switch

---
+++ Aula03/Switch02/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Switch02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float num1, num2, resultado = 0;
+            int opcao;
+
+            Console.WriteLine("############# CALCULADORA #############");
+
+            Console.WriteLine("Digite o primeiro número: ");
+            num1 = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            num2 = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Selecione um dos operadores abaixo para obter o resultado: ");
+            Console.WriteLine(" ----- 1. Soma -----");
+            Console.WriteLine(" ----- 2. Subtração -----");
+            Console.WriteLine(" ----- 3. Multiplicação ------");
+            Console.WriteLine(" ----- 4. Divisão ----- ");
+
+            opcao = Convert.ToInt32(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 1: 
+                    resultado = num1 + num2;
+                    break;
+
+                case 2:
+                    resultado = num1 -num2;
+                    break;
+
+                case 3:
+                    resultado = num1 *num2;
+                    break;
+
+                case 4:
+                    resultado = num1 /num2;
+                    break;
+            }
+
+            Console.WriteLine($"O resultado é: {resultado}");   
+
+        }
+    }
+}
