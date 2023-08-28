abeb10235e6475e96e575a40e0e8ea66b6fae8c4
mnqrs <116473798+mnqrs@users.noreply.github.com>
2023-08-28 15:27:38 -0300

    Vetores 02

---
+++ Aula05/ExVetores02/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace ExVetores02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = new int[10];
+            float soma = 0;
+            float media = 0;
+
+            Console.WriteLine("Digite os números: ");
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                vetor[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                Console.WriteLine($"vetor[{i}] = {vetor[i]}");
+            }
+
+            Console.WriteLine("Elementos nos índices ímpares: ");
+            for(int i = 0; i < vetor.Length; i++)
+
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(vetor[i] + " ");
+                }
+            }
+
+            Console.WriteLine("Elementos pares: ");
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                if (vetor[i] % 2 == 0)
+                {
+                    Console.WriteLine(vetor[i] + " ");
+                }
+            }
+
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                soma += vetor[i];
+                media = soma / 10;
+            }
+            Console.WriteLine($"O valor da soma dos elementos é {soma} e o valor da média é {media}.");
+        }
+    }
+}
